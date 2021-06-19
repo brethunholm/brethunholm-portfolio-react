8578a7cf59e853804ed4cd04fdb5811c4335b176
Bre Thunholm <bthunholm@gmail.com>
2021-06-19 10:46:35 -0500

    updated portfolio item data

--- src/components/portfolioPageData.js
+++ src/components/portfolioPageData.js
@@ -18,10 +18,9 @@ const data = {
         bottomParagraph: `
 
         This page has many accessibility features including:
+        Responsive, uses grid template area to change layout for mobile experience. Countdown clock written with javascript, custom rsvp form created using formik. Image gallery and registry. Blog style guest book.
         
-        Effective use of color, large color contrast makes the page easy for people to see and read.
-        Accessible with keyboard, a large bright active border makes it clear what the user is on the page.
-        Good use of HTML headings, allows users with screen readers to navigate the page according to its headings and skip or move the the users desired content.`,
+        `,
         src: '',
         img: 'images/wedding.jpg'
     },
@@ -29,7 +28,7 @@ const data = {
         projectTitle: `Meditation`,
         projectLink: `https://namagomeditate.com/`,
         projectSubtitle: `bringing Mindfulness to others`,
-        headerParagraph: `I built and designed this meditation website to help bring mindfulness to others.  The webpage has 4 different Mantras for users to repeat to themselves. Each Page contains 23 different mantras that appear in random order along with a photo.  The idea is users can scroll through the page, repeating the mantras to themselves, to bring them back into the present moment, help them move beyond their fear, or put a new positive twist on their day and lives. `,
+        headerParagraph: `Self guided meditation resource.  The webpage has 4 different Mantras for users to repeat to themselves. Built with React and CSS Flexbox.  `,
         bottomParagraph: ` NamaGoMeditate uses a dropdown hamburger navigation menu that has a position of fixed, allowing it to always be with the user.  The hamburger menu has the NamaGo logo, allows users to go back to the top of the page and navigate to other pages.`,
         src: 'https://www.youtube.com/embed/cgx6tuvOQYU?autoplay=1',
         img: 'images/meditation.jpg'
