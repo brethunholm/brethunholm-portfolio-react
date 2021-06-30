84fcba5a95c66328405ace6d09bf481c0fab3239
Bre Thunholm <bthunholm@gmail.com>
2021-06-30 15:34:52 -0500

    added contact-me modal

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from 'react';
+import './ContactMe.css';
 
-export default function ContactMe() {
+export default function ContactMe({ show, close }) {
     return (
-        <div className="contact">
-             <a href="mailto:bthunholm@gmail.com" className="contact__link">bthunholm@gmail.com</a>
-             <a className="contact__link" href="https://github.com/brethunholm">
-             github.com/brethunholm
-                        </a>
+        <div className="contact-card"
+            style={{
+                transform: show ? 'translateY(0vh)' : 'TranslateY(-100vh)',
+                opacity: show ? '1' : '0'
+            }}
+        >
+            <div className="content">
+  <div className="card">
+    <div className="card__side card__side--front">
+      {/* <!-- Front Content --> */}
+      <div className="card__cont">
+        <span className="blue">alert</span>
+        <span>(<span className="green">'Contact Bre Thunholm'</span>)</span>
+      </div>
+    </div>
+    <div className="card__side card__side--back">
+      {/* <!-- Back Content --> */}
+      <div className="card__cta">
+      <button onClick={close} className="btn-cancel">x</button>
+        <p><span className="purple">const</span> aboutMe <span className="cyan">=</span> &#123;
+          <br />
+          <span className="space red">name</span>
+          <span className="cyan">:</span> <span className="green">'bre thunholm'</span>,
+          <br/>
+          <span className="space red">email</span>
+          <span className="cyan">:</span>  <a href="mailto:bthunholm@gmail.com" className="green">'bthunholm@gmail.com'</a>,
+          <br/>
+          <span className="space red">resume</span>
+          <span className="cyan">:</span>  <a href="https://app.box.com/s/rv3cjoxhq171kr3b242xiv1rc90fv949" className="green" target="_blank" rel="noopener noreferrer">'thunholm-resume'</a>,
+          <br />
+          <span className="space red">github</span><span className="cyan">:</span> <a className="green" href="https://github.com/brethunholm">'github.com/brethunholm'</a>
+          <br/> 
+          &#125;;
+        </p>
+      </div>
+    </div>
+   
+  </div>
+        </div>
+        
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
+import ContactMe from './ContactMe';
 
 export default function Header() {
+   const [ show, setShow ] = useState(false);
+
+  function closeModalHandler() {
+      setShow(false)
+  }
+
    const [ isOpen, setIsOpen ] = useState('');
    function toggleNav() {
        !isOpen ? setIsOpen('nav-open') : setIsOpen('')
        document.body.classList.toggle('nav-open')
    }
 
-
-
     return (
         
             <header>
+                {/* { show ? <div onClick={closeModalHandler} className="back-drop"></div> : null} */}
                 <div className="resume">
-                    <a href="https://app.box.com/s/rv3cjoxhq171kr3b242xiv1rc90fv949" target="_blank" rel="noopener noreferrer">&lt; Resume / &gt;</a>
+                    {/* <a href="https://app.box.com/s/rv3cjoxhq171kr3b242xiv1rc90fv949" target="_blank" rel="noopener noreferrer">&lt; Resume / &gt;</a> */}
+                    <button onClick={() => setShow(true)}className="btn-openModal">&lt; contact me / &gt;</button>
+                <ContactMe show={show} close={closeModalHandler}/>
                 </div>
 
         <button onClick={toggleNav} className={`nav-toggle ${isOpen}`} aria-label="toggle navigation">
