5f26a99c0c514779594f87f689142795eb99ac44
Bre Thunholm <bthunholm@gmail.com>
2022-05-10 15:15:36 -0500

    update images and colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,61 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ContactMe from './ContactMe';
 
 export default function Header() {
-   const [ show, setShow ] = useState(false);
+  const [show, setShow] = useState(false);
 
   function closeModalHandler() {
-      setShow(false)
+    setShow(false);
   }
 
-   const [ isOpen, setIsOpen ] = useState('');
-   function toggleNav() {
-       !isOpen ? setIsOpen('nav-open') : setIsOpen('')
-       document.body.classList.toggle('nav-open')
-   }
+  const [isOpen, setIsOpen] = useState('');
+  function toggleNav() {
+    !isOpen ? setIsOpen('nav-open') : setIsOpen('');
+    document.body.classList.toggle('nav-open');
+  }
 
-    return (
-        
-            <header>
-                {/* { show ? <div onClick={closeModalHandler} className="back-drop"></div> : null} */}
-                <div className="resume">
-                    {/* <a href="https://app.box.com/s/rv3cjoxhq171kr3b242xiv1rc90fv949" target="_blank" rel="noopener noreferrer">&lt; Resume / &gt;</a> */}
-                    <button onClick={() => setShow(true)}className="btn-openModal">&lt; contact me / &gt;</button>
-                <ContactMe show={show} close={closeModalHandler}/>
-                </div>
-
-        <button onClick={toggleNav} className={`nav-toggle ${isOpen}`} aria-label="toggle navigation">
-            <span className="hamburger"></span>
+  return (
+    <header>
+      {/* { show ? <div onClick={closeModalHandler} className="back-drop"></div> : null} */}
+      <div className="resume">
+        {/* <a href="https://app.box.com/s/rv3cjoxhq171kr3b242xiv1rc90fv949" target="_blank" rel="noopener noreferrer">&lt; Resume / &gt;</a> */}
+        <button onClick={() => setShow(true)} className="btn-openModal">
+          &lt; contact me / &gt;
         </button>
-        <nav className="nav">
-            <ul onClick={toggleNav} className="nav__list">
-                <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-                <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
-                <li className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
-                <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
-            </ul>
-        </nav>
+        <ContactMe show={show} close={closeModalHandler} />
+      </div>
+
+      <button
+        onClick={toggleNav}
+        className={`nav-toggle ${isOpen}`}
+        aria-label="toggle navigation"
+      >
+        <span className="hamburger"></span>
+      </button>
+      <nav className="nav">
+        <ul onClick={toggleNav} className="nav__list">
+          <li className="nav__item">
+            <a href="#home" className="nav__link">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#services" className="nav__link">
+              My Services
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#about" className="nav__link">
+              About Me
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#work" className="nav__link">
+              My Work
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
-        
-    )
+  );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Intro.css';
 
-
 export default function Intro() {
-
-    return (
-        <div>
-              <section class="intro" id="home">
+  return (
+    <div>
+      <section class="intro" id="home">
         <h1 class="section__title section__title--intro">
-            Hi, I am <strong>Bre Thunholm</strong>
+          Hi, I am <strong>Bre Thunholm</strong>
         </h1>
-        <p class="section__subtitle section__subtitle--intro">front-end developer</p>
-        <img src="images/bre-main.png" alt="Bre Thunholm" class="intro__img home" />
-    </section>
-        </div>
-    )
+        <p class="section__subtitle section__subtitle--intro">
+          frontend developer
+        </p>
+        <img
+          src="images/bre-main.png"
+          alt="Bre Thunholm"
+          class="intro__img home"
+        />
+      </section>
+    </div>
+  );
 }
